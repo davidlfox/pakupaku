b617501d8fa6335277710f1f5c1666179892036a
David Fox <me@iamdavidfox.com>
2021-02-15 15:12:56 -0500

    wire up the POST

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import render_template
-from flask import Flask
+from flask import Flask, render_template, jsonify
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/fortune", methods=["POST"])
+def fortune():
+    # todo: get state
+    # respond/validate accordingly
+    return jsonify({"message": "the message"})
